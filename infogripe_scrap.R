b979d5f91b91c33b8d0fa000eb700a655e94d973
adriancom <adrianobf@gmail.com>
2020-04-15 02:12:57 -0300

    update scapper

--- infogripe_scrap.R
+++ infogripe_scrap.R
@@ -4,21 +4,33 @@
 
 library(tidyverse)
 
-df <- data.frame()
+df = data.frame()
 
 for (ano in 2009:2020){
-  #SE = Semana Epidemiológicaa
+  
+  #SE = Semana Epidemiológica (epiweek)
   for (SE in 1:53){
-    print(paste('baixando a SE',SE,"do ano",ano))
     
+    print(paste('Acessando a SE',SE,"do ano",ano))
     url = stringr::str_glue('http://info.gripe.fiocruz.br/data/detailed/1/2/{ano}/{SE}/1/Brasil/data-table')
-    base <- jsonlite::fromJSON(url) %>% purrr::pluck("data") %>% mutate(casos = str_extract(value,"[0-9]+")) %>% mutate(ano = ano)
-    df <- rbind(df,base)
+    dados_json = jsonlite::fromJSON(url) %>% purrr::pluck("data") 
+    
+    # Solicitar a SE 53 para anos que não a possuem geram JSON nulos. Não os queremos.
+    if (!is.null(dados_json)) { 
+    dados = dados_json %>% mutate(ano = ano)
+    }
+    df = rbind(df,dados)
   }
 }
 
-ordem_colunas <- c("ano","epiweek", "territory_name","casos", "situation_name","value")
+# Vamos remover registros duplicados. E criar um campo numérico para os casos.
+df = df[!duplicated(df),] %>% mutate(casos = as.integer(str_extract(value,"[0-9]+"))) 
 
-df <- df[, ordem_colunas] %>% filter(territory_name != 'Brasil') %>% arrange(-ano)
+# E separar 2 datasets. Um nacional agregado e outro por UF.
+df_brasil = df %>% filter(territory_name == 'Brasil') 
+df = df %>% filter(territory_name != 'Brasil') 
 
-write_csv(df,"dados.csv")
+write_csv(df[,c("ano","epiweek","casos","territory_name", "situation_name","value")],"dados_uf.csv")
+
+write_csv(df[,c("ano","epiweek","casos","territory_name", "situation_name","value")],"dados_br.csv")
+
