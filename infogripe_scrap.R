0e33c40622ccd23375a334efb0a38da2f8f671c6
adriancom <adrianobf@gmail.com>
2020-04-20 13:27:31 -0300

    fix error epiweek

--- infogripe_scrap.R
+++ infogripe_scrap.R
@@ -4,13 +4,17 @@ library(tidyverse)
 semana = 1:53
 ano = 2009:2020
 url_base = 'http://info.gripe.fiocruz.br/data/detailed/1/2/'
+#estados = read.csv('https://raw.githubusercontent.com/belisards/dados_auxiliares/master/UFs.csv')
 
 # Gera os links, criando todas as combinações de semanas (1:53) e anos (2018:2020), no formato da URL
 url_casos = paste(url_base,"%s/%s/1/Brasil/","data-table", sep = '')
 url_detalhe = paste(url_base,"%s/%s/Brasil/","age-distribution", sep = '')
+#url_detalheUF = paste(url_base,"%s/%s/%s/","age-distribution",sep='')
 
 lista_casos = do.call(sprintf, c(expand.grid(ano,semana), fmt = url_casos))
 lista_detalhe = do.call(sprintf, c(expand.grid(ano,semana), fmt = url_detalhe))
+#lista_detalheUF = do.call(sprintf, c(expand.grid(ano,semana,estados$estado), fmt = url_detalheUF))
+
 
 ##################### FUNÇÕES ############################################
 # Ao raspar os dados, descataremos JSON nulos e vamos adicionar um campo com a URL de onde foram extraídos.
@@ -26,8 +30,8 @@ raspa_casos = function(link){
   }
 }
 
-# Para ler os dados de faixa etária
-regex_epiweek = '(?<=\\d{4}\\/)\\d'
+# Para ler os dados de faixa etária no Brasil
+regex_epiweek = '(?<=\\d{4}\\/)\\d+'
 
 raspa_detalhe = function(link){
   print(paste("Acessando a URL: ",link))
@@ -39,10 +43,13 @@ raspa_detalhe = function(link){
   }
 }
 
+
 ##################### SCRAP ############################################
 ## DETALHADO POR GÊNERO E FAIXA ETÁRIA
 # Para raspar os dados de faixa etária, podemos ler como CSV diretamente. 
 df_detalhe = bind_rows(lapply(lista_detalhe,raspa_detalhe)) 
+
+unique(df_detalhe$epiweek)
 
 # CASOS TOTAIS POR ESTADO E NACIONAL
 # Cria a base de casos e adiciona um campo numérico limpo com os casos totais
