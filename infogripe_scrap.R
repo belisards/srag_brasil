92ea5f571783fb65ed714d3f484734d5d7f659e7
adriancom <adrianobf@gmail.com>
2020-04-21 01:11:20 -0300

    update script

--- infogripe_scrap.R
+++ infogripe_scrap.R
@@ -1,20 +1,20 @@
 # Script para extrair dados de SRAG do site Infogripe - http://info.gripe.fiocruz.br/
+
 library(tidyverse)
 
 ##################### PARÂMETROS GERAIS ############################################
 semana = 1:53
-ano = 2009:2020
+anos = 2009:2020
 url_base = 'http://info.gripe.fiocruz.br/data/detailed/1/2/'
-estados = read.csv('https://raw.githubusercontent.com/belisards/dados_auxiliares/master/UFs.csv')
+estados = read.csv('https://raw.githubusercontent.com/belisards/dados_auxiliares/master/UFs.csv') %>% select(estado)
 
 # Gera os links, criando todas as combinações de semanas (1:53) e anos (2018:2020), no formato da URL
 url_casos = paste(url_base,"%s/%s/1/Brasil/","data-table", sep = '')
 url_detalhe = paste(url_base,"%s/%s/Brasil/","age-distribution", sep = '')
-url_detalheUF = paste(url_base,"%s/%s/%s/","age-distribution",sep='')
 
-lista_casos = do.call(sprintf, c(expand.grid(ano,semana), fmt = url_casos))
-lista_detalhe = do.call(sprintf, c(expand.grid(ano,semana), fmt = url_detalhe))
-s
+lista_casos = do.call(sprintf, c(expand.grid(anos,semana), fmt = url_casos))
+lista_detalhe = do.call(sprintf, c(expand.grid(anos,semana), fmt = url_detalhe))
+
 
 ##################### FUNÇÕES ############################################
 # Ao raspar os dados, descataremos JSON nulos e vamos adicionar um campo com a URL de onde foram extraídos.
@@ -44,12 +44,42 @@ raspa_detalhe = function(link){
 }
 
 
+
 ##################### SCRAP ############################################
-## DETALHADO POR GÊNERO E FAIXA ETÁRIA
+
+# DETALHADO POR GÊNERO E FAIXA ETÁRIA POR UF
+
+df_detalheUF = tibble()
+baixadas = NA
+
+# Isso aqui está feio e futuramente pode melhorar de muitas formas, por ex, lidando melhor com erros de requisição no servidor
+# No momento, é preciso rodar/checar "manualmente", leva um bom tempo e caso ocorra um erro é preciso rodar novamente o loop. 
+# Ele irá pular as semanas já armazenadas. 
+for (uf in estados$estado){
+  for (ano in anos){
+    for (epiweek in semana){
+      link = paste(url_base,ano,"/",epiweek,"/",uf,"/age-distribution",sep='')
+      if(link %in% baixadas){
+        print(paste("OK: Semana",epiweek,"do ano de",ano,"do",URLdecode(uf)))
+      } else {
+        print(paste("Download: Semana",epiweek,"do ano de",ano,"do",URLdecode(uf)))
+          dado = read.csv(link) %>% mutate(url = link) %>%
+            mutate(ano = ano) %>%
+            mutate(epiweek = epiweek) %>%
+            mutate(estado = URLdecode(uf))
+        
+          df_detalheUF = bind_rows(dado,df_detalheUF)
+          
+          baixadas = unique(df_detalheUF$url)
+        
+              }
+    }
+  }
+}
+
+## DETALHADO POR GÊNERO E FAIXA ETÁRIA NO BRASIL
 # Para raspar os dados de faixa etária, podemos ler como CSV diretamente. 
 df_detalhe = bind_rows(lapply(lista_detalhe,raspa_detalhe)) 
-
-unique(df_detalhe$epiweek)
 
 # CASOS TOTAIS POR ESTADO E NACIONAL
 # Cria a base de casos e adiciona um campo numérico limpo com os casos totais
@@ -60,12 +90,14 @@ mutate(casos = as.integer(str_extract(value,"[0-9]+")))
 df_casos_br = df_casos %>% filter(territory_name == 'Brasil') 
 df_casos_uf = df_casos %>% filter(territory_name != 'Brasil') 
 
-
-##################### EXPORTAÇÃO ############################################
 # Vamos reordenar as colunas na hora de exportar os dados para facilitar a leitura
 ordem_colunas = c("ano","epiweek","casos","territory_name", "situation_name","value")
 
+
+##################### EXPORTAÇÃO ############################################
 # Exporta CSV
 write_csv(df_casos_uf[,ordem_colunas],"casos_uf.csv")
 write_csv(df_casos_br[,ordem_colunas],"casos_br.csv")
 write_csv(df_detalhe,"detalhe_br.csv")
+write_csv(df_detalheUF,"detalhe_uf.csv")
+
